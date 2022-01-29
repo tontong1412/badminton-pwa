38013f203fe22176de2fe5b8d6d14793b5fcaccb
Tontong <tontongfordev@gmail.com>
2022-01-29 20:53:43 +0700

    login before register tournament

--- components/Tournament/SlipModal.js
+++ components/Tournament/SlipModal.js
@@ -100,7 +100,7 @@ const SlipModal = ({ event, team = {}, visible, setVisible, mutate, isManager })
         </div>
         <div style={{ display: 'flex', gap: '10px' }}>
           <div style={{ fontWeight: 'bold', width: '80px' }}>ผู้จัดการทีม:</div>
-          <div>{team?.contact.officialName}</div>
+          <div>{team?.contact?.officialName}</div>
         </div>
         <div style={{ display: 'flex', gap: '10px' }}>
           <div style={{ fontWeight: 'bold', width: '80px' }}>ค่าสมัคร:</div>
--- pages/tournament/[id]/participants.js
+++ pages/tournament/[id]/participants.js
@@ -1,16 +1,18 @@
 import Layout from '../../../components/Layout/tournamentManager'
 import { useRouter } from 'next/router'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { TAB_OPTIONS } from '../../../constant'
 import { useEffect, useState } from 'react'
 import ParticipantsTable from '../../../components/TournamentManager/participants'
 import RegisterModal from '../../../components/Tournament/RegisterModal'
 import AddButton from '../../../components/addButton'
+import { Modal } from 'antd'
 
 const Participants = (props) => {
   const dispatch = useDispatch()
   const router = useRouter()
   const { id } = router.query
+  const { user } = useSelector(state => state)
   const [registerModalVisible, setRegisterModalVisible] = useState(false)
   useEffect(() => {
     dispatch({ type: 'ACTIVE_MENU', payload: TAB_OPTIONS.TOURNAMENT_MANAGER.PARTICIPANTS })
@@ -19,7 +21,15 @@ const Participants = (props) => {
     <Layout>
       <h1>ตรวจสอบรายชื่อ</h1>
       <ParticipantsTable tournamentID={id} />
-      <AddButton onClick={() => setRegisterModalVisible(true)} />
+      <AddButton onClick={() => {
+        if (user.id) setRegisterModalVisible(true)
+        else {
+          Modal.info({
+            title: 'กรุณา Log in ก่อนสมัครแข่งขัน',
+            onOk: () => router.push('/login')
+          })
+        }
+      }} />
       <RegisterModal
         visible={registerModalVisible}
         setVisible={setRegisterModalVisible}
