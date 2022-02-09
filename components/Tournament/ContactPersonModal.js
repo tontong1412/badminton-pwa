b7ac7ca761f778a363f875f19413808e5afea3b7
Tontong <tontongfordev@gmail.com>
2022-02-10 01:05:30 +0700

    undefined contact

--- components/Tournament/ContactPersonModal.js
+++ components/Tournament/ContactPersonModal.js
@@ -13,51 +13,54 @@ const ContactPerson = ({ player = {}, visible, setVisible, showContact }) => {
       centered
       footer={null}
     >
-      <div style={{
-        width: '100%',
-        padding: '50px 0',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        textAlign: 'center'
-      }}>
-        <div style={{
-          width: '80px',
-          height: '80px',
-          borderRadius: '40px',
-          overflow: 'hidden',
-          objectFit: 'contain',
-          marginTop: '10px'
+      {player._id
+        ? <div style={{
+          width: '100%',
+          padding: '50px 0',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          textAlign: 'center'
         }}>
-          <Image objectFit='cover' src={player.photo || `/avatar.png`} alt='' width={50} height={50} layout='responsive' />
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', gap: '5px' }}>
-          <div >{player.officialName}</div>
-          {player?.displayName && <div>({player?.displayName})</div>}
-        </div>
-        <div>{`${player.club}`}</div>
+          <div style={{
+            width: '80px',
+            height: '80px',
+            borderRadius: '40px',
+            overflow: 'hidden',
+            objectFit: 'contain',
+            marginTop: '10px'
+          }}>
+            <Image objectFit='cover' src={player.photo || `/avatar.png`} alt='' width={50} height={50} layout='responsive' />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px', gap: '5px' }}>
+            <div >{player.officialName}</div>
+            {player?.displayName && <div>({player?.displayName})</div>}
+          </div>
+          {player.club && <div>{`${player.club}`}</div>}
 
-        {
-          showContact && <>
-            <div style={{ display: 'flex', gap: '10px', width: '220px' }}>
-              <div style={{ width: '100px', textAlign: 'right' }}>เบอร์โทรศัพท์:</div>
-              <div><a href={`tel:${player?.tel}`}>{player?.tel}</a></div>
-              <div onClick={() => {
-                copy(player.tel)
-                message.success('copied')
-              }} style={{ color: COLOR.MINOR_THEME }}><CopyOutlined /></div>
-            </div>
-            <div style={{ display: 'flex', gap: '10px', width: '220px' }}>
-              <div style={{ width: '100px', textAlign: 'right' }}>Line ID:</div>
-              <div>{player?.lineID}</div>
-              <div onClick={() => {
-                copy(player?.lineID)
-                message.success('copied')
-              }} style={{ color: COLOR.MINOR_THEME }}><CopyOutlined /></div>
-            </div>
-          </>
-        }
-      </div >
+          {
+            showContact && <>
+              <div style={{ display: 'flex', gap: '10px', width: '220px' }}>
+                <div style={{ width: '100px', textAlign: 'right' }}>เบอร์โทรศัพท์:</div>
+                <div><a href={`tel:${player?.tel}`}>{player?.tel}</a></div>
+                {player.tel && <div onClick={() => {
+                  copy(player.tel)
+                  message.success('copied')
+                }} style={{ color: COLOR.MINOR_THEME }}><CopyOutlined /></div>}
+              </div>
+              <div style={{ display: 'flex', gap: '10px', width: '220px' }}>
+                <div style={{ width: '100px', textAlign: 'right' }}>Line ID:</div>
+                <div>{player?.lineID}</div>
+                {player?.lineID && <div onClick={() => {
+                  copy(player?.lineID)
+                  message.success('copied')
+                }} style={{ color: COLOR.MINOR_THEME }}><CopyOutlined /></div>}
+              </div>
+            </>
+          }
+        </div >
+        : <div>ไม่มีข้อมูลผู้ติดต่อ</div>
+      }
     </Modal>
   )
 }
