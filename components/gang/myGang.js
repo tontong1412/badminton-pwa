9487102d770d19ef80eb5c7633335af798b05dda
Tontong <tontongfordev@gmail.com>
2022-04-12 21:32:28 +0700

    my gang use swr

--- components/gang/myGang.js
+++ components/gang/myGang.js
@@ -1,36 +1,11 @@
-import { useEffect, useState, forwardRef, useImperativeHandle } from "react"
 import { useSelector } from 'react-redux'
 import MyGangCard from "../myGangCard"
-import request from "../../utils/request"
+import { useMyGang } from "../../utils"
 import { Divider } from "antd"
-const MyGang = (props, ref) => {
+const MyGang = (props) => {
   const user = useSelector(state => state.user)
-  const [myGang, setMyGang] = useState()
-
-  useEffect(() => {
-    if (user.token) {
-      fetchData()
-    }
-  }, [user])
-
-  useImperativeHandle(ref, () => ({
-    fetchMyGang() {
-      fetchData()
-    }
-  }), [])
-
-  const fetchData = () => {
-    if (user.token) {
-      request.get(`/gang/my-gang`,
-        { token: user.token }
-      ).then(res => {
-        setMyGang(res.data)
-      })
-        .catch(() => { })
-    }
-  }
-  if (!myGang || myGang.length === 0) return <div />
-
+  const { myGangs } = useMyGang(user.token)
+  if (!myGangs || myGangs.length === 0) return <div />
   return (
     <>
       <div style={{ margin: '20px 20px 0 20px' }}>ก๊วนของฉัน</div>
@@ -40,7 +15,7 @@ const MyGang = (props, ref) => {
         display: 'flex',
         marginLeft: '5px'
       }}>
-        {myGang?.length > 0 && myGang?.map(gang => {
+        {myGangs?.length > 0 && myGangs?.map(gang => {
           return <MyGangCard key={`mygang-card-${gang._id}`} gang={gang} style={{ float: 'right' }} />
         })
         }
@@ -48,6 +23,5 @@ const MyGang = (props, ref) => {
       {props.bottomLine && <Divider />}
     </>
   )
-
 }
-export default forwardRef(MyGang)
+export default MyGang
