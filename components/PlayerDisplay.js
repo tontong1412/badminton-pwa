9bc4e1774abd862d67214bfd1ee4b297b1b6fe5a
Nonnadda <>
2022-12-04 23:18:02 +0700

    show recentactivity tournament participant

--- components/PlayerDisplay.js
+++ components/PlayerDisplay.js
@@ -1,12 +1,23 @@
-import { Popover } from 'antd'
+import { useState, useEffect } from 'react'
+import request from '../utils/request'
+import { Popover, Tag } from 'antd'
 import moment from 'moment'
 import Image from 'next/image'
 import { MAP_GENDER } from '../constant'
 const PlayerDisplay = ({ player, showContact }) => {
+  const [activity, setActivity] = useState()
+  useEffect(() => {
+    const getRecentActivity = async () => {
+      const recentActivity = await request.get(`/player/${player._id}/recent-activity`)
+      setActivity(recentActivity.data)
+    }
+    getRecentActivity()
+  }, [])
+
   const playerDetail = (
     <div style={{
-      width: '200px',
-      height: '250px',
+      width: '250px',
+      height: '350px',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
@@ -27,14 +38,31 @@ const PlayerDisplay = ({ player, showContact }) => {
       </div>
       <div>{`${player.club}`}</div>
 
-      <div style={{ display: 'flex', gap: '10px', width: '200px' }}>
-        <div style={{ width: '100px', textAlign: 'right' }}>อายุ:</div>
-        <div>{moment().diff(player.birthDate, 'year')}</div>
-      </div>
-      <div style={{ display: 'flex', gap: '10px', width: '200px' }}>
+      {moment().diff(player.birthDate, 'year') ?
+        <div style={{ display: 'flex', gap: '10px', width: '200px' }}>
+          <div style={{ width: '100px', textAlign: 'right' }}>อายุ:</div>
+          <div>{moment().diff(player.birthDate, 'year')}</div>
+        </div> : null}
+
+      {player.gender && <div style={{ display: 'flex', gap: '10px', width: '200px' }}>
         <div style={{ width: '100px', textAlign: 'right' }}>เพศ:</div>
         <div>{MAP_GENDER[player.gender]}</div>
-      </div>
+      </div>}
+      {activity?.length > 0 &&
+        <div style={{ width: '100%' }}>
+          <div style={{ borderBottom: '1px solid #eee', padding: '5px', width: '100%', textAlign: 'center' }}>รายการแข่งล่าสุด</div>
+          {
+            activity.map(t =>
+              <div key={t?._id} >
+                <div
+                  style={{ borderBottom: '1px solid #eee', padding: '5px', width: '100%', textAlign: 'center' }}>
+                  {t?.name}
+                  {t?.events.map(e => <Tag key={e?._id} color='blue' style={{ marginLeft: '3px' }}>{e?.name}</Tag>)}
+                </div>
+              </div>)
+          }
+        </div>
+      }
       {
         showContact && <>
           <div style={{ display: 'flex', gap: '10px', width: '200px' }}>
