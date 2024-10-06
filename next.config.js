325ba02725d31f6ea3f822a83f66c087db33283c
Nonnadda <tontongfordev@gmail.com>
2024-10-06 16:43:42 +0300

    upgrade next to 12

--- next.config.js
+++ next.config.js
@@ -1,35 +1,34 @@
-require('dotenv').config();
+// next.config.js
+
 const withAntdLess = require('next-plugin-antd-less');
-const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-  env: {
-    spaceID: process.env.spaceID,
-    accessTokenDelivery: process.env.accessTokenDelivery,
-  },
+  reactStrictMode: true,
   distDir: '.next',
   images: {
     domains: ['res.cloudinary.com'],
   },
+  env: {
+    spaceID: process.env.spaceID,
+    accessTokenDelivery: process.env.accessTokenDelivery,
+  },
+  webpack(config) {
+    // Add any custom webpack configurations here
+    return config;
+  },
 };
 
-const plugins = [
-  withAntdLess({
-    // optional
-    modifyVars: {
-      '@primary-color': '#80644f',
-      '@font-family': "IBM Plex Sans Thai, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans- serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
-    },
-    // optional
-    lessVarsFilePathAppendToEndOfContent: false,
-    // optional https://github.com/webpack-contrib/css-loader#object
-    cssLoaderOptions: {},
-
-    // Other Config Here...
-    webpack(config) {
-      return config;
-    },
-  })
-]
-
-module.exports = withPlugins(plugins, nextConfig);
+module.exports = withAntdLess({
+  ...nextConfig,
+  modifyVars: {
+    '@primary-color': '#80644f',
+    '@font-family':
+      "IBM Plex Sans Thai, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+  },
+  lessVarsFilePathAppendToEndOfContent: false,
+  cssLoaderOptions: {},
+  webpack(config) {
+    // If you have additional webpack customizations, add them here
+    return config;
+  },
+});
